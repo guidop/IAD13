152dc14e4d743e132e279522cc63a353b7621590
guidop <guido.pederzini@gmail.com>
2013-11-21 22:44:36 +0100

    migliorato output mem leak listener

--- IAD13.Tests/stdafx.h
+++ IAD13.Tests/stdafx.h
@@ -13,4 +13,23 @@
 
 #pragma warning (disable:4100)
 
-// TODO: reference additional headers your program requires here
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#define _CRTDBG_MAP_ALLOC
+
+/* Memory block identification */
+//#define _FREE_BLOCK      0
+//#define _NORMAL_BLOCK    1
+//#define _CRT_BLOCK       2
+//#define _IGNORE_BLOCK    3
+//#define _CLIENT_BLOCK    4
+//#define _MAX_BLOCKS      5
+
+#ifdef _DEBUG
+#ifndef DBG_NEW
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#define new DBG_NEW
+#endif
+#endif  
+//
